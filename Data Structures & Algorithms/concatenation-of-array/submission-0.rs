a42b10060fe7b633fb51738cffe18707a6651a8d
Manav Kapil <manavkapil11@gmail.com>
2026-07-01 21:41:52 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans: Vec<i32> = vec![];
+
+        for i in 0..nums.len(){
+            ans.push(nums[i])
+        } 
+
+        for i in 0..nums.len(){
+            ans.push(nums[i])
+        } 
+        ans
+        
+    }
+}
