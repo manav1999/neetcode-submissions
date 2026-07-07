94d0d8de7adf8ab35e410cf16173154e8733d811
Manav Kapil <manavkapil11@gmail.com>
2026-07-07 22:24:25 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+
+        if s.len() != t.len() {
+            return false;
+        }
+
+   let mut counts: HashMap<char, i32> = HashMap::new();
+
+    for c in s.chars() {
+        *counts.entry(c).or_insert(0) += 1;
+    }
+    
+    for k in t.chars(){
+        if let Some(count) = counts.get_mut(&k) {
+            *count -= 1;
+             if *count == 0 {
+             counts.remove(&k);
+    
+                            }
+
+        }
+    }
+
+    if counts.is_empty() {
+  return true;
+}
+false
+
+    }
+}
